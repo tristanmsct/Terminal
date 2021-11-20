85128ef8d5e84952749987f33dad1e78763b0dcc
Tristan Muscat <tristan.muscat@gmail.com>
2021-11-20 15:48:51 +0100

    Changed project name to a free one.

---
+++ src/inputmanager/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Created on Fri Jul 17 08:50:55 2020.
+
+@author: Tristan Muscat
+"""
+from .inputmanager import force_read
+from .inputmanager import InputException
+from .inputmanager import read_line
+from .inputmanager import read_numeric
+from .inputmanager import read_yes_no
