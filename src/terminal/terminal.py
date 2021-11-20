8b6a2f73803e727f5251c204bf205e6db8dc569b
Tristan Muscat <tristan.muscat@gmail.com>
2021-11-20 14:18:01 +0100

    Restructured the repo to handle automation for a code formater, linter, mypy, pytest, etc.

--- src/terminal/terminal.py
+++ src/terminal/terminal.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Tue Apr 23 21:58:12 2019.
 
 @author: Tristan Muscat
 """
-
 # =============================================================================
 # Libraries
 # =============================================================================
-
-from typing import Union, Optional
-from collections.abc import Callable, Iterable
+from collections.abc import Callable
+from collections.abc import Iterable
+from typing import Optional
+from typing import Union
 
 # =====================================================================================================================
 # Error class specific to typos
@@ -22,7 +21,7 @@ class InputException(Exception):
     """Typing error."""
 
     def __init__(self, message):
-        super(InputException, self).__init__(message)
+        super().__init__(message)
 
 
 # =====================================================================================================================
@@ -33,7 +32,7 @@ class InputException(Exception):
 def read_line(
     str_prompt: str,
     bl_case: Optional[bool] = False,
-    set_values: Iterable[Union[str, float, bool]] = None,
+    set_values: Optional[Iterable[Union[str, float, bool]]] = None,
 ) -> str:
     """Read an input from the user after a prompt.
 
@@ -160,20 +159,16 @@ def read_numeric(
         except ValueError:
             raise InputException("Input must be numeric.")
 
-    if num_ub is not None and (
-        (bl_inc_ub and res > num_ub) or (not bl_inc_ub and res >= num_ub)
-    ):
+    if num_ub is not None and ((bl_inc_ub and res > num_ub) or (not bl_inc_ub and res >= num_ub)):
         raise InputException("Expected a smaller input.")
 
-    if num_lb is not None and (
-        (bl_inc_lb and res < num_lb) or (not bl_inc_lb and res <= num_lb)
-    ):
+    if num_lb is not None and ((bl_inc_lb and res < num_lb) or (not bl_inc_lb and res <= num_lb)):
         raise InputException("Expected a bigger input.")
 
     return res
 
 
-def force_read(fun_reader: Callable, *argv, **kwargs) -> Union[str, float, bool]:
+def force_read(fun_reader: Callable[..., Union[str, float, bool]], *argv, **kwargs) -> Union[str, float, bool]:
     """Loop until it gets a valid inputs.
 
     Ask an input and ask again if their is an error. It allows a program not to crash because of a typo.
--- tests/test_terminal.py
+++ tests/test_terminal.py
@@ -1,41 +1,40 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Tue Aug 11 11:50:14 2020
 
 @author: Tristan Muscat
 """
-
-from terminal import InputManager as im
-from terminal import InputException
 import unittest  # For unit testing
 from unittest.mock import patch
+
+import terminal as im
+from terminal import InputException
 
 
 class TestTerminal(unittest.TestCase):
     @patch("builtins.input", return_value="Hello")
     def test_readline(self, input):
-        self.assertEqual(im.read_line("Input", bl_case=False, lst_values=None), "Hello")
+        self.assertEqual(im.read_line("Input", bl_case=False, set_values=None), "Hello")
 
     @patch("builtins.input", return_value="hello")
     def test_readline_case(self, input):
         with self.assertRaises(InputException):
-            im.read_line("Input", bl_case=True, lst_values=["Hello"])
+            im.read_line("Input", bl_case=True, set_values=["Hello"])
 
     @patch("builtins.input", return_value="other")
     def test_readline_intput_not_in_expected_values(self, input):
         with self.assertRaises(InputException):
-            im.read_line("Input", bl_case=True, lst_values=["Hello"])
+            im.read_line("Input", bl_case=True, set_values=["Hello"])
 
-    @patch("terminal.InputManager.read_line", return_value="y")
+    @patch("builtins.input", return_value="y")
     def test_yesno(self, input):
         self.assertTrue(im.read_yes_no("Input"))
 
-    @patch("terminal.InputManager.read_line", return_value=15)
+    @patch("builtins.input", return_value=15)
     def test_read_numeric_isequal(self, input):
         self.assertEqual(im.read_numeric("Input"), 15)
 
-    @patch("terminal.InputManager.read_line", return_value=15)
+    @patch("builtins.input", return_value=15)
     def test_read_numeric_ub(self, input):
         with self.assertRaises(InputException):
             im.read_numeric(
@@ -47,7 +46,7 @@ class TestTerminal(unittest.TestCase):
                 bl_inc_ub=True,
             )
 
-    @patch("terminal.InputManager.read_line", return_value=10)
+    @patch("builtins.input", return_value=10)
     def test_read_numeric_ub_2(self, input):
         with self.assertRaises(InputException):
             im.read_numeric(
@@ -59,7 +58,7 @@ class TestTerminal(unittest.TestCase):
                 bl_inc_ub=False,
             )
 
-    @patch("terminal.InputManager.read_line", return_value=-5)
+    @patch("builtins.input", return_value=-5)
     def test_read_numeric_lb(self, input):
         with self.assertRaises(InputException):
             im.read_numeric(
@@ -71,7 +70,7 @@ class TestTerminal(unittest.TestCase):
                 bl_inc_ub=True,
             )
 
-    @patch("terminal.InputManager.read_line", return_value=0)
+    @patch("builtins.input", return_value=0)
     def test_read_numeric_lb_2(self, input):
         with self.assertRaises(InputException):
             im.read_numeric(
